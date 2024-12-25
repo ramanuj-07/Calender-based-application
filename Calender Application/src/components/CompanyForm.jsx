2af744d9d9d2d52d5f335baa62597914dce0cb9d
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-12-25 17:14:41 +0530

    Update CompanyForm.jsx
    Company Mgmt section.
    setting up the necessary hooks

--- Calender Application/src/components/CompanyForm.jsx
+++ Calender Application/src/components/CompanyForm.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 
-const CompanyForm = () => {
+const CompanyForm = ({ onSubmit }) => {
+    const [company, setCompany] = useState({});
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCompany({ ...company, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(company);
+        setCompany({});
+    }
+
   return (
       <>
         
